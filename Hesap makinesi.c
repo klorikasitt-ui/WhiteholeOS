0b946ab54e8cc80ba9919b7fa83d4ed624cbf780
BURAK YAKUB GÜÇER <klorikasitt@gmail.com>
2026-03-23 01:32:40 +0300

    Implement basic calculator in C
    Implement a basic calculator with addition, subtraction, and multiplication operations. The program continuously prompts the user for input until an exit command is given.

---
+++ Hesap makinesi.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+while(1)
+{
+int x, y, islem;
+printf("Bilgi kutusu islem icin (not islem girin yazan yer gelince gecerlidir)1 Toplama, 2 Cikarma 3 Carpma\n");
+printf("bir sayi gir\n");
+scanf("%d" , &x);
+printf("ikinci sayiyi gir\n");
+scanf("%d" , &y);
+printf("islem girin\n");
+scanf("%d" , &islem);
+if (islem == 1) {
+    printf("Sonuc: %d\n", x + y);
+}
+else if (islem == 2) {
+    printf("Sonuc: %d\n", x - y);
+}
+else if (islem == 3) {
+    printf("Sonuc: %d\n", x * y);
+}
+if (islem == 0) {
+    break;
+}
+
+}
+}
